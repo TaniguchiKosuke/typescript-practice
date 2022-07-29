65f6617a26f1a5be14d321187db1ec693932d3d4
Kosuke Taniguchi <73885532+TaniguchiKosuke@users.noreply.github.com>
2022-07-29 17:34:22 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Person = /** @class */ (function () {
+    function Person(name, age, nationality) {
+        this.name = name;
+        this.age = age;
+        this.nationality = nationality;
+    }
+    Person.prototype.walk = function () {
+        return "".concat(this.name, " is walking");
+    };
+    Person.prototype.profile = function () {
+        return "My name is ".concat(this.name, ". I am ").concat(this.age, " years old");
+    };
+    return Person;
+}());
+var SuperPerson = /** @class */ (function (_super) {
+    __extends(SuperPerson, _super);
+    function SuperPerson(name, age, nationality, skill) {
+        var _this = _super.call(this, name, age, nationality) || this;
+        _this.skill = skill;
+        return _this;
+    }
+    SuperPerson.prototype.AboutSuperPerson = function () {
+        return "".concat(this.name, " && ").concat(this.nationality, " && ").concat(this.skill);
+    };
+    return SuperPerson;
+}(Person));
+var kosuke = new Person("kosuke", 21, "Japan");
+console.log(kosuke.walk());
+console.log(kosuke.profile());
+var superkosuke = new SuperPerson("kosuke", 21, "Japan", "I am a software engineer");
+console.log(superkosuke.AboutSuperPerson());
+function buildName(firstName, lastName) {
+    if (lastName === void 0) { lastName = "Taniguchi"; }
+    return firstName + " " + lastName;
+}
+console.log(buildName("Kosuke"));
+console.log(buildName("Kosuke", "Suzuki"));
+function returnVoid() {
+    console.log("return void");
+}
+returnVoid();
---
+++ main.ts
@@ -0,0 +1,64 @@
+class Person {
+    public name: string
+    private age: number
+    protected nationality: string
+
+    constructor(name: string, age: number, nationality: string) {
+        this.name = name
+        this.age = age
+        this.nationality = nationality
+    }
+
+    walk(): string {
+        return `${this.name} is walking`
+    }
+
+    profile(): string {
+        return `My name is ${this.name}. I am ${this.age} years old`
+    }
+}
+
+class SuperPerson extends Person {
+    skill: string
+    constructor(name: string, age: number, nationality: string, skill: string) {
+        super(name, age, nationality)
+        this.skill = skill
+    }
+
+    AboutSuperPerson() {
+        return `${this.name} && ${this.nationality} && ${this.skill}`
+    }
+}
+
+let kosuke = new Person("kosuke", 21, "Japan")
+console.log(kosuke.walk())
+console.log(kosuke.profile())
+let superkosuke = new SuperPerson("kosuke", 21, "Japan", "I am a software engineer")
+console.log(superkosuke.AboutSuperPerson())
+
+
+function buildName(firstName: string, lastName="Taniguchi"): string {
+    return firstName + " " + lastName
+}
+
+console.log(buildName("Kosuke"))
+console.log(buildName("Kosuke", "Suzuki"))
+
+function returnVoid(): void{
+    console.log("return void")
+}
+
+returnVoid()
+
+interface MyFoo {
+    method: ()=> void;
+  }
+  
+  class Foo {
+    method(): void {
+      console.log('Hello, world!');
+    }
+  }
+  
+  const obj: MyFoo = new Foo();
+  const obj2: Foo = obj;
